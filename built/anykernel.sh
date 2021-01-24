fe5787d72679623e56c69a9642dd9984c05d26bc
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-01-24 21:50:39 +0800

    AnyKernel3 Updated
    * Updated to https://github.com/osm0sis/AnyKernel3/commit/d1fd1eb01380749c030071d0eb54fcaffc7d74e5
    * Add Android Version 11
    * Removed Unused Overdose Init

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose R24 - #LoveOD
+kernel.string=Overdose R25 - #LoveOD
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,8 @@ device.name2=X00TD
 device.name3=X00T
 device.name4=ASUS_X00T
 device.name5=ASUS_X00TDA
-supported.versions=8.1 - 10.0
+supported.versions=8.1 - 11.0
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
@@ -39,35 +40,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 dump_boot;
 
 # begin ramdisk changes
-
-# Backup init.rc
-backup_file init.rc;
-
-# Overdose's Ramdisk Removal & Injection
-rm -rf $ramdisk/init.trb.rc;
-rm -rf $ramdisk/init.azure.rc;
-rm -rf $ramdisk/init.Pbh.rc;
-rm -rf $ramdisk/init.PbH.rc;
-rm -rf $ramdisk/init.darkonah.rc;
-rm -rf $ramdisk/init.spectrum.rc;
-rm -rf $ramdisk/init.spectrum.sh;
-rm -rf $ramdisk/init.special_power.sh;
-rm -rf $ramdisk/init.thundercloud.rc;
-rm -rf $ramdisk/init.thundercloud.sh;
-
-remove_line init.rc "import /init.trb.rc"
-remove_line init.rc "import /init.azure.rc"
-remove_line init.rc "import /init.PbH.rc"
-remove_line init.rc "import /init.Pbh.rc"
-remove_line init.rc "import /init.darkonah.rc"
-remove_line init.rc "import /init.overdose.rc"
-remove_line init.rc "import /init.thundercloud.rc"
-remove_line init.rc "import /init.spectrum.rc"
-remove_line init.rc "import /init.pk.rc"
-remove_line init.rc "import /init.boost.rc"
-remove_line init.rc "import /init.stardust.rc"
-insert_line init.rc "import /init.overdose.rc" after "import /init.usb.configfs.rc" "import /init.overdose.rc";
-
 # end ramdisk changes
 
 write_boot;
