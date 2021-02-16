3a70403a7ec21d090be8da8c441192c0f8c34f3e
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-02-16 14:45:05 +0800

    Overdose R25S Release

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose R25 - #LoveOD
+kernel.string=Overdose R25S - #LoveOD
 do.devicecheck=1
 do.modules=0
 do.systemless=1
